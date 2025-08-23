fe5c7d9fe6e30517b5d8e23df0eca61d5efdad4e
jonahmdelaney <jonah.m.delaney@gmail.com>
2025-08-23 18:35:53 -0500

    Update darkmode.js

--- darkmode.js
+++ darkmode.js
@@ -4,7 +4,6 @@ function toggleTheme() {
   document.documentElement.setAttribute("data-theme", next);
   localStorage.setItem("theme", next);
 }
-
 (function () {
   const saved = localStorage.getItem("theme") || "light";
   document.documentElement.setAttribute("data-theme", saved);
